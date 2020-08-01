d7f6154c57225da5788fbdc2ce3414731f9f0736
Soham Sarkar <sohamsarkar.work@gmail.com>
2020-08-01 09:46:54 +0530

    added StringStream

---
+++ StringStream.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> v;
+    char c;
+    int number;
+    do{
+        ss>>number;
+        v.push_back(number);
+        
+    }while(ss>>c);
+    
+    
+        
+    return v; 
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
